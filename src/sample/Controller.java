6b169a74690a04a2962bf5dec3950d0d077997e3
ManfredBuron <manfred.buron1@gmail.com>
2020-06-15 10:37:47 +0200

    some adds

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,24 @@
 package sample;
 
-public class Controller {
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+/**
+ * Address :
+ * ENSICAEN
+ * 6 Boulevard Maréchal Juin
+ * F-14050 Caen Cedex
+ *
+ * Note :
+ * This file is owned by an ENSICAEN student.  No portion of this
+ * document may be reproduced, copied  or revised without written
+ * permission of the authors.
+ *
+ * @author BURON Manfred <manfred.buron@ecole.ensicaen.fr>
+ * @author Bahia SECHI <bahia.sechi@ecole.ensicaen.fr>
+ * @version 1.0
+ */
+
+public class Controller implements Initializable{
 }
